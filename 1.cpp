9a28be2b93b8e433376768e60ce70df886920c25
MaosenDing <378910982@qq.com>
2021-05-14 10:10:01 +0800

    同步测试

--- 1.cpp
+++ 1.cpp
@@ -1,1 +1,23 @@
-789789789789
+Git 全局设置:
+ 
+git config --global user.name "ASxx" 
+git config --global user.email "123456789@qq.com"
+ 
+创建 git 仓库:
+ 
+mkdir wap // 项目在本地的路径
+cd wap
+git init 
+touch README.md 
+git add README.md 
+git commit -m "first commit" 
+git remote add origin https://github.com/MaosenDing/vscode.git  // 远程仓库地址
+git push -u origin master
+ 
+ 
+ 
+已有项目：
+ 
+cd existing_git_repo
+git remote add origin https://github.com/MaosenDing/vscode.git
+git push -u origin master
