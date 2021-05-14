f865e070549ebef41442d7fd4eae1555ebd8c901
MaosenDing <39754398+MaosenDing@users.noreply.github.com>
2021-05-14 10:23:58 +0800

    Update 2.cpp
    克隆同步测试

--- sn1/2.cpp
+++ sn1/2.cpp
@@ -1,1 +1,14 @@
-1313543
+克隆同步测试
+
+
+git config --global user.name "ASxx"
+git config --global user.email "123456789@qq.com"  
+ 
+git config --global credential.helper store   
+ 
+ 
+ 
+#然后打开Git Bash输入以下命令
+ 
+cd d:/test   //指定存放的目录
+git clone https://git.oschina.net/name/test.git   //你的仓库地址
